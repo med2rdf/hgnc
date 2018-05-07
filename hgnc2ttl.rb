ee9ec6013f8e5ced313bf89dac8d370da7e14534
Toshiaki Katayama <k@bioruby.org>
2018-05-07 11:46:48 +0900

    Fixed bug of LRG URI genration and udpated data

--- hgnc2ttl.rb
+++ hgnc2ttl.rb
@@ -40,6 +40,9 @@ class HGNC2TTL
   def xref(db, ids, predicate = 'rdfs:seeAlso')
     if ids and not ids.empty?
       ids.gsub('"','').split('|').each do |id|
+        if db == "lrg"
+          next unless id[/^LRG_/]
+        end
         uri = "<http://identifiers.org/#{db}/#{id}>"
         puts triple(@subject, predicate, uri)
         puts triple(uri, "rdf:type", "<http://identifiers.org/#{db}>")
