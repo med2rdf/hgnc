113f7d39a8d37a5e3ca475165ff107b0e7499ede
Toshiaki Katayama <k@bioruby.org>
2020-04-09 15:50:12 +0900

    Added obo:SO_0000704 (gene) type in addition to m2r:Gene, assinged Id.org DB type to Id.org URIs in xref, introduced cytoband from location with obo:so_part_of

--- hgnc2ttl.rb
+++ hgnc2ttl.rb
@@ -24,47 +24,63 @@ class HGNC2TTL
     ["@prefix", "rdfs:", "<http://www.w3.org/2000/01/rdf-schema#>"],
     ["@prefix", "dct:", "<http://purl.org/dc/terms/>"],
     ["@prefix", "skos:", "<http://www.w3.org/2004/02/skos/core#>"],
+    ["@prefix", "obo:", "<http://purl.obolibrary.org/obo/>"],
     ["@prefix", "m2r:", "<http://med2rdf.org/ontology/med2rdf#>"],
   ]
 
-  def type(uri, predicate = "rdf:type")
-    puts triple(@subject, predicate, uri)
+  def type(uri)
+    puts triple(@subject, "rdf:type", uri)
   end
 
-  def label(str, predicate = "rdfs:label")
-    puts triple(@subject, predicate, quote(str))
+  def label(str)
+    puts triple(@subject, "rdfs:label", quote(str))
   end
 
   def description(str)
     puts triple(@subject, "dct:description", quote(str))
   end
 
-  def seealso(uri, predicate = "rdfs:seeAlso")
-    puts triple(@subject, predicate, uri)
+  def location(str)
+    puts triple(@subject, "obo:so_part_of", quote(str))
   end
 
-  def reference(db, id)
-    xref(db, id, "dct:references")
+  def alt_label(str)
+    alias_names(str, "skos:altLabel")
   end
 
-  def alias_names(str)
-    str.gsub!('"', '')
-    ary = str.split('|')
-    ary.each do |item|
-      label(item, "skos:altLabel") unless item.empty?
+  def alias_names(str, predicate = "skos:altLabel")
+    split_ids(str).each do |item|
+      unless item.empty?
+        puts triple(@subject, predicate, quote(item))
+      end
     end
   end
 
-  def xref(db, ids, predicate = 'rdfs:seeAlso')
-    if ids and not ids.empty?
-      ids.gsub('"','').split('|').each do |id|
-        if db == "lrg"
-          next unless id[/^LRG_/]
-        end
-        uri = "<http://identifiers.org/#{db}/#{id}>"
-        seealso(uri)
-        type("<http://identifiers.org/#{db}>")
+  def see_also(db, ids)
+    xref(db, ids, "rdfs:seeAlso")
+  end
+
+  def reference(db, ids)
+    xref(db, ids, "dct:references")
+  end
+
+  def xref(db, str, predicate = 'rdfs:seeAlso')
+    split_ids(str).each do |id|
+      if db == "lrg"
+        next unless id[/^LRG_/]
       end
+      db_uri = "<http://identifiers.org/#{db}>"
+      id_uri = "<http://identifiers.org/#{db}/#{id}>"
+      puts triple(@subject, predicate, id_uri)
+      puts triple(id_uri, "rdf:type", db_uri)
+    end
+  end
+
+  def split_ids(ids)
+    if ids and not ids.empty?
+      ids.gsub('"','').split('|')
+    else
+      []
     end
   end
 
@@ -135,26 +151,28 @@ class HGNC2TTL
   def parse_line(line)
     ary = line.strip.split("\t")
     @subject = "<http://identifiers.org/hgnc/#{ary[0].sub('HGNC:', '')}>"
+    type("obo:SO_0000704")
     type("m2r:Gene")
     label(ary[1])
     description(ary[2])
-    alias_names(ary[8])
-    alias_names(ary[9])
-    xref("ncbigene", ary[18])
-    xref("ensembl", ary[19])
-    xref("ena.embl", ary[22])
-    xref("refseq", ary[23])
-    xref("ccds", ary[24])
-    xref("uniprot", ary[25])
+    location(ary[6])
+    alt_label(ary[8])
+    alt_label(ary[9])
+    see_also("ncbigene", ary[18])
+    see_also("ensembl", ary[19])
+    see_also("ena.embl", ary[22])
+    see_also("refseq", ary[23])
+    see_also("ccds", ary[24])
+    see_also("uniprot", ary[25])
     reference("pubmed", ary[26])
-    xref("mgi", ary[27])
-    xref("rgd", ary[28].sub('RGD:','')) if ary[28]
-    xref("lrg", ary[29])
-    xref("omim", ary[31])
-    xref("mirbase", ary[32])
-    xref("orphanet", ary[36])
-    xref("iuphar.receptor", ary[41])
-    xref("ec-code", ary[46])
+    see_also("mgi", ary[27])
+    see_also("rgd", ary[28].sub('RGD:','')) if ary[28]
+    see_also("lrg", ary[29])
+    see_also("omim", ary[31])
+    see_also("mirbase", ary[32])
+    see_also("orphanet", ary[36])
+    see_also("iuphar.receptor", ary[41])
+    see_also("ec-code", ary[46])
     puts
   end
 end
